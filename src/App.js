ed7ff068d1a3b0b0d5b080f492657b48e997e16f
aferrio <adriano.ferrio@gmail.com>
2022-12-22 10:55:13 +0100

    Message

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>This is the part of modified code from Adriano</p>
         <a
           className="App-link"
           href="https://reactjs.org"
